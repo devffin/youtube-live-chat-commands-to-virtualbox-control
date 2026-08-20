07d988df438a08492b02172a1de2ff6b3397478f
devffin <lessupersenfants.theo@gmail.com>
2026-08-20 22:10:50 +0200

    Remaked the script to work with requirements.txt

--- installreq.bat
+++ installreq.bat
@@ -1,15 +1,4 @@
-:start
-cls
+@echo off
 
-set python_ver=36
-
-python ./get-pip.py
-
-cd \
-cd \python%python_ver%\Scripts\
-pip install pytchat
-pip install json
-pip install os
-
+pip install -r requirements.txt
 pause
-exit
